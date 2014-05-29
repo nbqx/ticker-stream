9d04c12385c4211145abeb5a89e841bc9df5c3cd
n.b.q.x <nbqxnbq@gmail.com>
2014-05-29 15:43:42 +0900

    add test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
 var test = require('tape');
 var ticker = require(__dirname+'/..');
 
+var start,
+    end;
+
 function testStream(d){
   var stream = new require('stream').Readable({objectMode:true});
   stream.push(d);
@@ -13,5 +16,37 @@ test('get data after 1000ms',function(t){
   var wait = 1000;
   var st = testStream('abcd');
   var tick = ticker(wait);
-  t.skip('TODO!!');
+
+  tick.on('readable',function(){
+    start = Date.now();
+  });
+  
+  tick.on('finish',function(){
+    end = Date.now();
+    // 10ms or less as error
+    var it_takes = Math.floor((end-start)/10)*10;
+    t.equal(wait,it_takes);
+  });
+
+  st.pipe(tick);
+});
+
+test('get object after 1000ms',function(t){
+  t.plan(1);
+  var wait = {data: 'abcd', duration: 1000};
+  var st = testStream(wait);
+  var tick = ticker({wait: 'duration'});
+
+  tick.on('readable',function(){
+    start = Date.now();
+  });
+  
+  tick.on('finish',function(){
+    end = Date.now();
+    // 10ms or less as error
+    var it_takes = Math.floor((end-start)/10)*10;
+    t.equal(wait.duration,it_takes);
+  });
+
+  st.pipe(tick);
 });
